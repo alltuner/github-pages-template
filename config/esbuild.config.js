d3c423e25edba18a2f1629cd175f29299efd3f97
David Poblador i Garcia <david@poblador.com>
2025-03-07 11:58:06 +0100

    include lucide icons

---
+++ config/esbuild.config.js
@@ -0,0 +1,11 @@
+import { createIcons } from "lucide";
+
+// Recommended way, to include only the icons you need.
+// Example:
+// import { Menu, ArrowRight, Globe } from "lucide";
+
+createIcons({
+  icons: {
+    // Include a Comma Separated List of Icons
+  },
+});
